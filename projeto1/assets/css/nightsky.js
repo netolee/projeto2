f864b4631633296a7180087e24e1766faee3b6c8
Eli Diniz da Silva Neto <54873267+netolee@users.noreply.github.com>
2019-12-24 16:08:49 -0300

    adicionar arquivos
    projeto site

---
+++ projeto1/assets/css/nightsky.js
@@ -0,0 +1,66 @@
+ j{
+    var xPos = 385;
+    var yPos = 23;
+    var yStar = 140;
+    var xStar = 1;
+    
+    draw = function() {
+        
+        yPos += 2;
+        xPos -= 1;
+        xStar += 2;
+        yStar += 5;
+        background(29, 40, 115);
+        fill(255, 242, 0);
+        ellipse(xPos, yPos, 10, 10);
+        
+        fill(255, 242, 0);
+        ellipse(xStar, yStar, 6, 6);
+        
+        fill(255, 242, 0);
+        ellipse(200, 200, 10, 10);
+        
+        fill(255, 242, 0);
+        ellipse(141, 231, 10, 10);
+        
+        fill(255, 242, 0);
+        ellipse(258, 173, 4, 4);
+        
+        fill(255, 242, 0);
+        ellipse(168, 156, 4, 4);
+        
+        fill(255, 242, 0);
+        ellipse(227, 137, 4, 4);
+        
+        fill(255, 242, 0);
+        ellipse(92, 92, 4, 4);
+        
+        fill(41, 20, 41);
+        rect(0, 336, 40, 90, 0);
+        
+        fill(41, 20, 41);
+        rect(100, 310, 40, 90, 0);
+        
+        fill(41, 20, 41);
+        rect(250, 328, 40, 90, 0);
+        
+        fill(41, 20, 41);
+        rect(40, 357, 40, 91, 7);
+        
+        fill(41, 20, 41);
+        rect(164, 337, 27, 90, 0);
+        
+        fill(41, 20, 41);
+        rect(297, 310, 19, 90, 0);
+        
+        fill(41, 20, 41);
+        rect(199, 360, 40, 90, 75);
+        
+        
+    
+    };
+    
+    
+
+
+}
